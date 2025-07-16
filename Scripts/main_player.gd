8863bc73c2929081cc3c9304386a81e72c408658
darker-knightRD <220646252+darker-knightRD@users.noreply.github.com>
2025-07-16 16:40:00 +0300

    Patched Move Speed and Jump Force
    Note: you need to reload the game so the changes happen and change the values ONLY from the scene not from the level scene cuz that will make it only change from that level and not the other levels

--- Scripts/main_player.gd
+++ Scripts/main_player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 @export var state_machine:Node
 @export var animations:AnimationPlayer
+@export var move_speed:float = 400.0
+@export var jump_force:float
 
 func _ready() -> void:
-	state_machine.init(self, animations)
+	state_machine.init(self, animations, move_speed, jump_force)
 
 func _unhandled_input(event: InputEvent) -> void:
 	state_machine.process_input(event)
